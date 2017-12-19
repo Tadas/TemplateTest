1c4693eed4bc7c770c52d52e09871071e3e534a7
Tadas Medisauskas <tadas.medisauskas@intelliflo.com>
2017-12-19 17:31:25 +0000

    Add Invoke-Build and some links

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ Add dependencies to .\Modules\ as git submodule
 
 # Build
 During build check out everything from git and zip it up (submodule commits need to be online
+
+# Reading
+
+ * http://duffney.io/GettingStartedWithInvokeBuild
+ * https://devblackops.io/building-a-simple-release-pipeline-in-powershell-using-psake-pester-and-psdeploy/#test
---
+++ TemplateTest.build.ps1
@@ -0,0 +1,36 @@
+﻿task InstallDependencies {
+	Install-Module Pester -Scope CurrentUser 
+	Install-Module PSScriptAnalyzer -Scope CurrentUser
+}
+
+task Analyze {
+	$scriptAnalyzerParams = @{
+		Path = "$BuildRoot\Begin.ps1"
+		Severity = @('Error', 'Warning')
+		Recurse = $true
+		Verbose = $false
+		# ExcludeRule = 'PSUseDeclaredVarsMoreThanAssignments'
+	}
+	
+	$Results = Invoke-ScriptAnalyzer @scriptAnalyzerParams
+
+	if ($Results) {
+		$Results | Format-Table
+		throw "One or more PSScriptAnalyzer errors/warnings where found."
+	}
+}
+
+task Test {
+	$invokePesterParams = @{
+		Strict = $true
+		PassThru = $true
+		Verbose = $false
+		EnableExit = $false
+	}
+
+	# Publish Test Results as NUnitXml
+	$testResults = Invoke-Pester @invokePesterParams;
+
+	$numberFails = $testResults.FailedCount
+	assert($numberFails -eq 0) ('Failed "{0}" unit tests.' -f $numberFails)
+}
